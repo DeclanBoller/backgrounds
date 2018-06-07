ee9a76c40ba61d2305444c110841a3c6766209ae
Declan Boller <declan.boller@hotmail.com>
2018-06-08 09:29:03 +1000

    Upload first JS attempt

---
+++ script.js
@@ -0,0 +1,30 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.getElementById("randomize");
+
+function randomGradient() {
+  color1.value = randomize();
+   color2.value = randomize();
+    setGradient();
+};
+
+function setGradient() {
+  body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  css.textContent = body.style.background + ";" ; 
+};
+
+function randomize() {
+    var color = '#';
+    var letters = '0123456789ABCDEF';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+window.onload = setGradient();
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+button.addEventListener("click", randomGradient);
